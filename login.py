7825dcbc3a7135656f3499d80858acd9661bef4c
srish <srishakatux@gmail.com>
2018-07-09 22:17:00 -0700

    Add code for sending post request to login

---
+++ login.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""
+    login.py
+
+    MediaWiki Action API Code Samples
+    Demo of `Login` module: Sending post request to login
+    MIT license
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+# Retrieve login token first
+PARAMS_0 = {
+    'action':"query",
+    'meta':"tokens",
+    'type':"login",
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS_0)
+DATA = R.json()
+
+LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+print(LOGIN_TOKEN)
+
+# Send a post request to login. Using the main account for login is not
+# supported. Obtain credentials via Special:BotPasswords
+# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+
+PARAMS_1 = {
+    'action':"login",
+    'lgname':"your_bot_username",
+    'lgpassword':"your_bot_password",
+    'lgtoken':LOGIN_TOKEN,
+    'format':"json"
+}
+
+R = S.post(URL, data=PARAMS_1)
+DATA = R.json()
+
+print(DATA)
