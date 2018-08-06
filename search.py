434821b221aff6902f79b306b8defb4e64e08adc
srish <srishakatux@gmail.com>
2018-08-06 02:08:01 -0700

    API:Search demo

---
+++ search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    search.py
+
+    MediaWiki Action API Code Samples
+    Demo of `Search` module: Search for a text or title
+    MIT license
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+SEARCHPAGE = "Nelson Mandela"
+
+PARAMS = {
+    'action':"query",
+    'list':"search",
+    'srsearch': SEARCHPAGE,
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+if DATA['query']['search'][0]['title'] == SEARCHPAGE:
+    print("Your search page '" + SEARCHPAGE + "' exists on English Wikipedia")
