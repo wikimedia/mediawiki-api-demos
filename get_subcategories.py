e3a2783302702673e39273f3a3157be94a214dbb
Supriya <suppu.themartian@gmail.com>
2018-10-08 12:17:09 +0530

    Documention for get categorymembers API

---
+++ get_subcategories.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    MediaWiki Action API Code Samples
+    Demo of `get_category_list` module : Sending get request to list pages that belong to a
+    given category
+    MIT license
+    """
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    'action': "query",
+    'list' : "categorymembers",
+    'cmtitle' : "Category:Wikipedia",
+    'cmtype' : "subcat",
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print DATA
