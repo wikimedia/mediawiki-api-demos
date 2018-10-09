c7470b14838a2126b14ddefbb263fcc2a7af1143
Supriya <suppu.themartian@gmail.com>
2018-10-09 12:22:50 +0530

    Formatting

--- get_recent_category_items.py
+++ get_recent_category_items.py
@@ -25,4 +25,4 @@ PARAMS = {
 R = S.get(url=URL, params=PARAMS)
 DATA = R.json()
 
-print DATA
+print(DATA)
--- get_subcategories.py
+++ get_subcategories.py
@@ -24,4 +24,4 @@ PARAMS = {
 R = S.get(url=URL, params=PARAMS)
 DATA = R.json()
 
-print DATA
+print(DATA)
