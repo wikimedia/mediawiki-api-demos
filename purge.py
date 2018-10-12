e623af96fc0511708eb04d4a09f769f84ef343f0
Didicodes <edidiongasikpo@gmail.com>
2018-10-12 12:49:20 +0100

    update purge.py

--- purge.py
+++ purge.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
+
 """ purge.py
     MediaWiki Action API Code Samples
-    Demo of `purge` module: Sending post request to purge cache of one page
+    Demo of `purge` module: Sending post request to  purge first 10 pages in the main namespace
     MIT license
 """
+
 import requests
+
 URL = 'https://en.wikipedia.org/w/api.php'
+
 PARAMS = {"action": "purge",
           "generator": "allpages",
           "gapnamespace": "0",
           "gaplimit": "10",
-          "format": "json"}
+          "format": "json"
+}
+
 RESPONSE = requests.post(URL, data=PARAMS)
+
 print(RESPONSE.text)
