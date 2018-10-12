31a90b1555837571854458138e3ff151263003da
Didicodes <edidiongasikpo@gmail.com>
2018-10-12 12:49:20 +0100

    Modified code samples for API:Purge

--- purge.py
+++ purge.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
+<<<<<<< HEAD
 """ purge.py
     MediaWiki Action API Code Samples
     Demo of `purge` module: Sending post request to  purge first 10 pages in the main namespace
+=======
+"""
+    purge.py
+    
+    MediaWiki Action API Code Samples
+    Demo of `purge` module: Sending post request to purge first 10 pages in the main namespace
+>>>>>>> Modified code samples for API:Purge
     MIT license
 """
 
 import requests
 
+<<<<<<< HEAD
 URL = 'https://en.wikipedia.org/w/api.php'
 
 PARAMS = {"action": "purge",
@@ -20,3 +29,19 @@ PARAMS = {"action": "purge",
 RESPONSE = requests.post(URL, data=PARAMS)
 
 print(RESPONSE.text)
+=======
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "purge",
+    "generator": "allpages",
+    "gapnamespace": "0",
+    "gaplimit": "10",
+    "format": "json"
+}
+
+R = requests.post(url=URL, data=PARAMS)
+print(R.text)
+>>>>>>> Modified code samples for API:Purge
