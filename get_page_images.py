40710bca5fb2f0fa72ba8a28fc96da9a3e2e3051
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-10-17 23:11:17 -0400

    ran pylint & fixed whitespace issues

--- get_page_images.py
+++ get_page_images.py
@@ -18,10 +18,10 @@ S = requests.Session()
 URL = "https://en.wikipedia.org/w/api.php"
 
 PARAMS = {
-  'action':'query',
-  'format':'json',
-  'prop':'images',
-  'titles':'Albert Einstein'
+    'action':'query',
+    'format':'json',
+    'prop':'images',
+    'titles':'Albert Einstein'
 }
 
 R = S.get(url=URL, params=PARAMS)
