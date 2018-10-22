2d53da1dd15afe0116e4ad9e8dd39020a9571d08
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-10-22 16:30:21 -0400

    added sample code for api:lists - get_25_category_pages.py

---
+++ get_25_category_pages.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+    get_10_category_pages.py
+    MediaWiki Action API Code Samples
+    Demo of `Lists` module
+    MIT license
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    'action':"query",
+    'format':"json",
+    'list': "categorymembers",
+    'cmtitle': "Category:Physics",
+    'cmlimit': 25
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
