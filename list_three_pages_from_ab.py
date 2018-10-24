7f024c99efb5c72b37533b7db6f27481fec8255b
Mumylulu123 <anitajimita@gmail.com>
2018-10-23 23:51:29 -0700

    Reviewed;API:AllPages_Code_Sample

--- list_three_pages_from_ab.py
+++ list_three_pages_from_ab.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """ get_pages.py
     MediaWiki Action API Code Samples
-    Sending GET request to obtain a JSON object listing all 5pages gotten, starting from "kre"
+    Sending GET request to obtain a JSON object listing pages of 3 categories
     MIT license
 """
 import requests
 
 S = requests.Session()
 URL = 'https://en.wikipedia.org/w/api.php'
-PARAMS = {"action":"query",
-          "list":"allpages",
-          "apfrom":"kre",
-          "aplimit":"5",
-          "format":"json"
+PARAMS = {"action": "query",
+          "list": "allpages",
+          "apfrom": "Ab",
+          "aplimit": "3",
+          "apnamespace":"14",
+          'format':"json"
           }
 R = S.get(url=URL, params=PARAMS)
 DATA = R.json()
