66c58969a2b32a8f3e797d52b8ad58f0406e707b
Mumylulu123 <anitajimita@gmail.com>
2018-10-23 23:08:54 -0700

    Reviewed;API:Parameter_Information_Code_Sample

--- Parameter_Information.py
+++ Parameter_Information.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 """ Parameter_Information.py
     MediaWiki Action API Code Samples
-    Sending GET request: Getting information about 
-    'action'='parse', 'prop'='info' query submodule, 
-    and a bogus query module
+    Sending GET request: Getting information about
+   'action'='parse', 'prop'='info' query submodule,  and a bogus query module
     MIT license
 """
 import requests
 
 S = requests.Session()
 URL = 'https://en.wikipedia.org/w/api.php'
-PARAMS = {"action": "paraminfo",
-          "modules": "parse|query+info|query+blah",
-          
-          'format':"json"
+PARAMS = {"action":"paraminfo",
+          "modules":"parse|query+info|query+blah",
+          "format":"json"
           }
 R = S.get(url=URL, params=PARAMS)
-DATA=R.json()
+DATA = R.json()
+
 print(DATA)
