d8334efec25b1d64ae11e11ed6a5c680fc41d96d
Mumylulu123 <anitajimita@gmail.com>
2018-10-24 02:48:38 -0700

    Pylint,API:Delete-Sample_Code

--- delete_page.py
+++ delete_page.py
@@ -4,9 +4,8 @@
     delete_page.py
 
     MediaWiki Action API Code Samples
-    'Delete' module:
-    1st Step;Fetching token of type `csrf`
-    2nd Step;Used fetched 'csrf_token' to delete a page
+    Send a Post request to delete a page on wikipedia.org
+    (Note that you will actually need adminstrative rights to perform this action, thanks!)
     MIT license
 """
 
@@ -16,12 +15,8 @@ S = requests.Session()
 
 URL = "https://test.wikipedia.org/w/api.php"
 
-PARAMS_0 = {
-    'action':"query",
-    'meta':"tokens",
-    'type':"csrf",
-    'format':"json"
-}
+#1st Step;Fetching token of type `csrf`
+PARAMS_0 = {"action":"query", "meta":"tokens", "type":"csrf", "format":"json"}
 
 R = S.get(url=URL, params=PARAMS_0)
 DATA = R.json()
@@ -30,9 +25,10 @@ CSRF_TOKEN = DATA['query']['tokens']['csrftoken']
 
 print(CSRF_TOKEN)
 
-#Use the csrf token in a post request to delete a page
+#2nd Step;Used fetched 'csrf_token' to delete a page in a post request
 
 PARAMS_1 = {"action":"delete", "title":"Rabosot", "token":CSRF_TOKEN, "format":"json"}
+
 R = S.post(URL, data=PARAMS_1)
 DATA = R.json()
 
