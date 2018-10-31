832ef82d41349fbb9a782f75f07ae8ee8dacc1a1
srish <srishakatux@gmail.com>
2018-10-31 22:50:51 +0530

    Simplify clientlogin; not use WTForms

--- clientlogin.py
+++ clientlogin.py
@@ -5,13 +5,14 @@
 
     MediaWiki Action API Code Samples
     Demo of `clientlogin` module: Sending post request to login
+
+    This demo app uses Flask (a Python web development framework).
+
     MIT license
-    Flask web form tutorial code borrowed from https://pythonspot.com/flask-web-forms/
 """
 
 import requests
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField
 
 S = requests.Session()
 URL = "https://en.wikipedia.org/w/api.php"
@@ -20,42 +21,36 @@ URL = "https://en.wikipedia.org/w/api.php"
 DEBUG = True
 APP = Flask(__name__)
 APP.config.from_object(__name__)
-APP.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+APP.config['SECRET_KEY'] = 'enter_your_secret_key'
 
-class MWLoginForm(Form):
-    """ Design MediaWiki Login form using Flask's Form class
-    """
-    name = TextField('Name:')
-    password = TextField('Password:')
 
 @APP.route("/", methods=['GET', 'POST'])
 def show_form():
-    """ Render form template and handle form submission request
-    """
-    form = MWLoginForm(request.form)
+    """ Render form template and handle form submission request """
 
     if request.method == 'POST':
-        name = request.form['name']
+        username = request.form['username']
         password = request.form['password']
-        start_client_login(name, password)
+        start_client_login(username, password)
 
-    return render_template('clientlogin_form.html', form=form)
+    return render_template('clientlogin_form.html')
 
-def start_client_login(name, password):
-    """ Fetch login token and send a request to client login
-    """
+def start_client_login(username, password):
+    """ Send a post request along with login token, user information
+    and return URL to the API to log in on a wiki """
+
     login_token = fetch_login_token()
 
-    resp = S.post(url=URL, data={
+    response = S.post(url=URL, data={
         'action': "clientlogin",
-        'username': name,
+        'username': username,
         'password': password,
-        'loginreturnurl': 'enter_a_url',
+        'loginreturnurl': 'http://127.0.0.1:5000/',
         'logintoken': login_token,
         'format': "json"
     })
 
-    data = resp.json()
+    data = response.json()
 
     if data['clientlogin']['status'] == 'PASS':
         flash('Login success! Welcome, ' + data['clientlogin']['username'] + '!')
@@ -63,16 +58,16 @@ def start_client_login(name, password):
         flash('Oops! Something went wrong -- ' + data['clientlogin']['messagecode'])
 
 def fetch_login_token():
-    """ Fetch login token
-    """
-    resp = S.get(
+    """ Fetch login token via `tokens` module """
+
+    response = S.get(
         url=URL,
         params={
             'action': "query",
             'meta': "tokens",
             'type': "login",
             'format': "json"})
-    data = resp.json()
+    data = response.json()
     return data['query']['tokens']['logintoken']
 
 if __name__ == "__main__":
