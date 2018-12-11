3a0b48d720b3082719332e0263c6e649cfb9a178
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-12-11 10:30:24 -0500

    added get_links.py

---
+++ python/get_links.py
@@ -0,0 +1,30 @@
+#This file is partly auto-generated
+
+#!/usr/bin/python3
+
+"""
+    get_links.py
+
+    MediaWiki Action API Code Samples
+    Demo of `Links` module: Get all links to the given page(s)
+
+    MIT License
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "query",
+    "format": "json",
+    "titles": "Albert Einstein",
+    "prop": "links"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
