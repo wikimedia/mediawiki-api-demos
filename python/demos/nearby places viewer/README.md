8d3a1a5e56a816d54849edaaeb66f7a2e6317822
srish <srishakatux@gmail.com>
2019-01-21 19:48:32 -0800

    Nearby places viewer demo app

---
+++ python/demos/nearby places viewer/README.md
@@ -0,0 +1,22 @@
+# Nearby Places Viewer
+
+Geo search for wiki pages near your location using the [Geolocation API](https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API) and MediaWiki Action API's [Geosearch](https://www.mediawiki.org/wiki/API:Geosearch) module. 
+
+Install
+-------
+```
+$ git clone https://github.com/srish/MediaWiki_Action_API_Code_Samples
+$ cd MediaWiki-Action-API-Code-Samples/python/demos/article suggestion
+Install the necessary python modules with pip
+$ python3 nearby.py
+```
+
+Screenshot
+----------
+<table><tr><td>
+<img src="screenshot.png" width="300" style="border 5px solid black">
+</td></tr></table>
+
+Credits
+-------
+[Nearby](https://github.com/prtksxna/pebble-nearby), a Pebble app.
---
+++ python/demos/nearby places viewer/nearby.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""
+    nearby.py
+
+    MediaWiki Action API Code Samples
+
+    Nearby places viewer app: Demo of geo search for wiki pages near a location using
+    the Geolocation API and MediaWiki Action API's Geosearch module.
+
+    MIT license
+"""
+
+from flask import Flask, request, render_template, jsonify
+import requests
+from haversine import haversine
+
+
+APP = Flask(__name__)
+SESSION = requests.Session()
+API_ENDPOINT = 'https://en.wikipedia.org/w/api.php'
+
+
+@APP.route('/', methods=['GET', 'POST'])
+def index():
+    """ Displays the index page accessible at '/'
+    """
+
+    if request.method == "POST":
+        data = request.get_json()
+        latitude = data['latitude']
+        longitude = data['longitude']
+
+        results = fetch_places_nearby(latitude, longitude)
+        return jsonify(results=results)
+
+    return render_template('places.html')
+
+def fetch_places_nearby(lat, lon):
+    """ Fetches nearby places via MediaWiki Action API's Geosearch module
+    """
+    params = {
+        "action": "query",
+        "prop": "coordinates|pageimages|description|info",
+        "inprop": "url",
+        "pithumbsize": 144,
+        "generator": "geosearch",
+        "ggsradius": 10000,
+        "ggslimit": 10,
+        "ggscoord": str(lat) + "|" + str(lon),
+        "format": "json",
+    }
+
+    res = SESSION.get(url=API_ENDPOINT, params=params)
+    data = res.json()
+    places = data['query'] and data['query']['pages']
+    results = []
+
+    for k in places:
+        title = places[k]['title']
+        description = places[k]['description'] if "description" in places[k] else ''
+        thumbnail = places[k]['thumbnail']['source'] if "thumbnail" in places[k] else ''
+        article_url = places[k]['fullurl']
+
+        cur_loc = (lat, lon)
+        place_loc = (places[k]['coordinates'][0]['lat'], places[k]['coordinates'][0]['lon'])
+
+        distance = round(haversine(cur_loc, place_loc, unit='mi'), 2)
+
+        results.append({
+            'title': title,
+            'description': description,
+            'thumbnail': thumbnail,
+            'articleUrl': article_url,
+            'distance': distance
+        })
+
+    return results
+
+if __name__ == '__main__':
+    APP.run(debug=True)
