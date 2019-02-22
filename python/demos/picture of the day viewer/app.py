3b1011e1d4deda5fb570190f645f7ce7b35cb220
Marty Hernandez Avedon <martyavedon@gmail.com>
2019-02-22 15:27:15 -0500

    linted & refactored app.py

--- python/demos/picture of the day viewer/app.py
+++ python/demos/picture of the day viewer/app.py
@@ -1,77 +1,154 @@
-from flask import Flask, render_template, request, jsonify, redirect
+"""
+    App.py
+    MediaWiki Action API Code Samples
+
+    Fetches the current Wikipedia Picture of the Day (POTD) and displays it on
+    a webpage. Also allows for user to change the date displayed.
+
+    Attributes
+    ----------
+        change_date(): Alter current date displayed
+        increment(date_object): Next date
+        decrement(date_object): Previous date
+        index(): Render web page with current POTD
+        potd_json(date_object): Create JSON from fetch_potd(day)
+        fetch_potd(day): Fetch data about current picture from POTD endpoint
+        fetch_image_info(file_name): Fetchs image & description url
+
+    MIT License
+"""
+
+#!/usr/bin/python3
+
 from datetime import date, timedelta
+from flask import Flask, render_template, request, jsonify, redirect
 import requests
 
 APP = Flask(__name__)
 SESSION = requests.Session()
 ENDPOINT = "https://en.wikipedia.org/w/api.php"
+CURRENT_DATE = date.today()
 
-current_date = date.today()
-
-@APP.route("/change_date", methods = ["POST"])
+@APP.route("/change_date", methods=["POST"])
 
 def change_date():
-    global current_date
+    """
+    Alter CURRENT_DATE in response to user input, thus changing the POTD
+    being displayed on the webpage.
 
-    try:
-        change_date = request.form["change_date"]
+    Returns
+    -------
+    Response object
+        A redirect to the index path, forcing the page to re-render with
+        the POTD for the updated CURRENT_DATE.
+    """
 
-        if (change_date == "← Back"):
-            new_date = decrement_date()
-        elif (change_date == "Next →"):
-            new_date = increment_date()
-    except:
-        new_date = date.today()
+    global CURRENT_DATE
 
-    current_date = new_date
+    user_input = request.form["change_date"]
+    new_date = CURRENT_DATE
+
+    if user_input == "← Back":
+        new_date = decrement(new_date)
+    elif user_input == "Next →":
+        new_date = increment(new_date)
+
+    CURRENT_DATE = new_date
 
     return redirect("/")
 
-def increment_date():
-    return current_date + timedelta(days = 1)
+def increment(date_object):
+    """Returns the next date for a date object (i.e. tomorrow)"""
 
-def decrement_date():
-    return current_date - timedelta(days = 1)
+    return date_object + timedelta(days=1)
 
-@APP.route("/", methods = ["GET", "POST"])
+def decrement(date_object):
+    """Returns the previous date for a date object (i.e. yesterday)"""
+
+    return date_object - timedelta(days=1)
+
+@APP.route("/", methods=["GET", "POST"])
 
 def index():
-    if (request.method == "POST"):
-        date_to_fetch = str(current_date)
-        results = fetch_potd(date_to_fetch)
+    """
+    Requests data from the WikiMedia Action API, related to the POTD for
+    the CURRENT_DATE, and renders it on a webpage located at the index path.
 
-        return jsonify(results = results)
+    Returns
+    -------
+    JSON data related to the current POTD, if POST request.
+    Renders the webpage, templates/index.html, otherwise.
+    """
+    if request.method == "POST":
+        return potd_json(CURRENT_DATE)
 
     return render_template("index.html")
 
-def fetch_potd(date):
+def potd_json(date_object):
+    """Returns JSON formated data for the current POTD"""
+
+    results = fetch_potd(date_object)
+
+    return jsonify(results=results)
+
+def fetch_potd(date_object):
+    """
+    Returns image data related to the current POTD.
+
+    Parameters
+    ----------
+    date_object: date
+        A datetime.date object, used to construct a title in order to find the
+        correct POTD in the POTD archives.
+
+    Returns
+    -------
+    list[dict]
+        JSON detailing the POTD's filename, image url, description url, and the
+        date it was featured as Picture of the Day.
+    """
+
+    day = date_object.isoformat()
+
     params = {
         "action": "query",
         "format": "json",
         "formatversion": "2",
         "prop": "images",
-        "titles": "Template:POTD protected/" + date
+        "titles": "Template:POTD protected/" + day
     }
 
-    response = SESSION.get(url = ENDPOINT, params = params)
+    response = SESSION.get(url=ENDPOINT, params=params)
     data = response.json()
 
-    try:
-        file_name = data["query"]["pages"][0]["images"][0]["title"]
-        image_info = fetch_image_info(file_name)
-    
-        results = [{
-            "title": file_name,
-            "image": image_info["image_url"],
-            "description": image_info["description_url"],
-            "date": date
-        }]
-    except:
-        return
+    file_name = data["query"]["pages"][0]["images"][0]["title"]
+    image_info = fetch_image_info(file_name)
+    formatted_day = date_object.strftime("%x")
+
+    results = [{
+        "title": file_name,
+        "image": image_info["image_url"],
+        "description": image_info["description_url"],
+        "date": formatted_day
+    }]
 
     return results
 
 def fetch_image_info(file_name):
+    """
+    Returns the POTD's image url & image description.
+
+    Parameters
+    ----------
+    File_name: String
+        The name of the image file for the POTD.
+
+    Returns
+    -------
+    list[dict]
+        Data detailing the POTD's image url and description url.
+    """
+
     params = {
         "action": "query",
         "format": "json",
@@ -80,14 +157,19 @@ def fetch_image_info(file_name):
         "titles": file_name
     }
 
-    response = SESSION.get(url = ENDPOINT, params = params)
+    response = SESSION.get(url=ENDPOINT, params=params)
     data = response.json()
     page = next(iter(data["query"]["pages"].values()))
     image_info = page["imageinfo"][0]
     image_url = image_info["url"]
     description_url = image_info["descriptionurl"]
 
-    return {"image_url": image_url, "description_url": description_url}
+    results = {
+        "image_url": image_url,
+        "description_url": description_url
+    }
+
+    return results
 
 if __name__ == "__main__":
     APP.run()
