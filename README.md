fda45fabd04491868dcc4cd8ffd78f5229302b7e
Abhishekism9450 <abhishekism9450@gmail.com>
2019-02-26 23:42:19 +0530

    documention for Compare API is added.

--- README.md
+++ README.md
@@ -73,6 +73,8 @@ Code snippets in Python demonstrating how to use various modules of the [MediaWi
   * [watch.py](python/watch.py): add a page to your watchlist 
 * [API:Alllinks](https://www.mediawiki.org/wiki/API:Alllinks)
   * [get_alllinks.py](python/get_alllinks.py): list links to a namespace
+* [API:Compare](https://www.mediawiki.org/wiki/API:Compare)
+  * [get_compare.py](python/get_compare.py): action used to compare between two titles.
 
 ### Search 
 * [API:Search](https://www.mediawiki.org/wiki/API:Search)
---
+++ python/compare.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    get_compare.py
+
+    MediaWiki Action API Code Samples
+    Demo of `Allpages` module: Get all pages whose title contains
+    the text "Jungle," in whole or part.
+
+    MIT License
+"""
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"	
+
+PARAMS = {
+    "action": "query",
+    "format": "json",
+    "action": "compare",
+   	"fromtitle":"Jungle",
+   	"totitle":"JunglePup",
+   	"prop":"ids|user|title"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
