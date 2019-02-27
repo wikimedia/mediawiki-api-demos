77b87081be8016acc375ec2d835d1acb216cfc29
eisheeta <eisheeta.barua@gmail.com>
2019-02-28 00:15:45 +0530

    Modified README.md to include information and links for API: Deletedrevs. Modified modules.json Added code sample file for API: Deletedrevs
    Modified README.md to include information and links for API: Deletedrevs.
    Modified modules.json
    Added code sample file for
    
    1. API: Deletedrevs

---
+++ python/get_deleted_revisions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+    get_deleted_revisions.py
+
+    MediaWiki Action API Code Samples
+    Demo of `...` module
+    MIT license
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    'action':"query",
+    'drstart':"20070904235959",
+    'drend':"20070904000000",
+    'list':"deletedrevs",
+    'drprop':"revid|user|minor|len|token",
+    'drlimit':"6",
+    'druser': "Catrope",
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+
