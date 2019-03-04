a43c964f4e190097fb948f6c2746845a15ed2c2a
srish <srishakatux@gmail.com>
2019-03-04 15:17:10 -0800

    minor fixes

--- python/get_deleted_revisions.py
+++ python/get_deleted_revisions.py
@@ -1,11 +1,14 @@
+#This file is auto-generated. See modules.json and autogenerator.py for details
+
 #!/usr/bin/python3
 
 """
     get_deleted_revisions.py
 
     MediaWiki Action API Code Samples
-    Demo of `...` module
-    MIT license
+    Demo of `Deletedrevs` module: List the six most recent deleted revisions from User:Catrope
+
+    MIT License
 """
 
 import requests
@@ -15,14 +18,14 @@ S = requests.Session()
 URL = "https://en.wikipedia.org/w/api.php"
 
 PARAMS = {
-    'action':"query",
-    'drstart':"20070904235959",
-    'drend':"20070904000000",
-    'list':"deletedrevs",
-    'drprop':"revid|user|minor|len|token",
-    'drlimit':"6",
-    'druser': "Catrope",
-    'format':"json"
+    "drend": "20070904000000",
+    "format": "json",
+    "druser": "Catrope",
+    "list": "deletedrevs",
+    "drstart": "20070904235959",
+    "drlimit": "6",
+    "drprop": "revid|user|minor|len|token",
+    "action": "query"
 }
 
 R = S.get(url=URL, params=PARAMS)
