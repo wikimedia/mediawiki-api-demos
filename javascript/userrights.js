23d96eb91d93a5cb554fca680e723f3fbd6b6ce6
Adarsh Patel <adarshpatel509@gmail.com>
2019-03-11 17:38:35 +0530

    Fix CORS errors.

--- javascript/userrights.js
+++ javascript/userrights.js
@@ -17,14 +17,14 @@ var params_1 = {
     "format": "json"
 };
 
-var query_url = url + "?origin=*";
+var query_url = url + "?";
 Object.keys(params_1).forEach(function(key){query_url += "&" + key + "=" + params_1[key];});
 
 fetch(query_url)
     .then(function(response){return response.json();})
     .then(function(response){
         var data = response;
-        var login_token = data.query.tokens.login_token;
+        var login_token = data.query.tokens.logintoken;
         
         // Step2: Send a post request to login. Use of main account for login is not
         //  supported. Obtain credentials via Special:BotPasswords
@@ -46,7 +46,6 @@ fetch(query_url)
                 "Content-Type": "application/json"
             }
             })
-            .then(function(response){return response.json();})
             .then(function(response){
                 // Step 3: Obtain a Userrights token
 
@@ -57,7 +56,7 @@ fetch(query_url)
                     "type": "userrights"
                 };
 
-                query_url = url + "?origin=*";
+                query_url = url + "?";
                 Object.keys(params_3).forEach(function(key){query_url += "&" + key + "=" + params_3[key];});
                 
                 fetch(query_url)
@@ -85,7 +84,6 @@ fetch(query_url)
                                 "Content-Type": "application/json"
                             }
                             })
-                            .then(function(response){return response.json();})
                             .then(function(response){
                                 var data = response;
                                 console.log(data);
