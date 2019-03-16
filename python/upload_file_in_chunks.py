83e791e75574ece235f443ab576f5dadbeb73e0c
mansiag <mansi.29ag@gmail.com>
2019-03-16 17:52:24 +0530

    Refactor code

--- python/upload_file_in_chunks.py
+++ python/upload_file_in_chunks.py
@@ -10,112 +10,124 @@
 import os
 import requests
 
+S = requests.Session()
+URL = "https://test.wikipedia.org/w/api.php"
+
+#filepath contains the path of the file's location
+FILE_PATH = 'f.jpg'
+FILE = open(FILE_PATH, 'rb')
+FILE_SIZE = os.stat(FILE_PATH).st_size
+
 def read_chunks(file_object, chunk_size=5000):
-    """Generator to return the next chunk"""
+    """Return the next chunk of the file."""
     while True:
         data = file_object.read(chunk_size)
         if not data:
             break
         yield data
 
-S = requests.Session()
-URL = "https://test.wikipedia.org/w/api.php"
+def fetch_login_token():
+    """Retrieve a login token."""
+    params = {
+        "action":"query",
+        "meta":"tokens",
+        "type":"login",
+        "format":"json"
+    }
+    res = S.get(url=URL, params=params)
+    data = res.json()
+    return data["query"]["tokens"]["logintoken"]
 
-#Step 1: Retrieve a login token
-PARAMS_1 = {
-    "action":"query",
-    "meta":"tokens",
-    "type":"login",
-    "format":"json"
-}
+def user_login(login_token):
+    """Send a post request to login."""
+    #Use of main account for login is not supported. Obtain credentials via Special:BotPasswords
+    #(https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+    params = {
+        "action":"login",
+        "lgname": "bot_username",
+        "lgpassword":"bot_password",
+        "format":"json",
+        "lgtoken": login_token
+    }
+    S.post(URL, data=params)
 
-R = S.get(url=URL, params=PARAMS_1)
-DATA = R.json()
+def fetch_csrf_token():
+    """While logged in, retrieve a CSRF token"""
+    params = {
+        "action": "query",
+        "meta":"tokens",
+        "format":"json"
+    }
+    res = S.get(url=URL, params=params)
+    data = res.json()
+    return data["query"]["tokens"]["csrftoken"]
 
-LOGIN_TOKEN = DATA["query"]["tokens"]["logintoken"]
+def upload_file_in_chunks(csrf_token):
+    """Send Multiple Post requests to upload a file in chunks using "stash" mode.
 
-# Step2: Send a post request to login. Use of main account for login is not
-# supported. Obtain credentials via Special:BotPasswords
-# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
-PARAMS_2 = {
-    "action":"login",
-    "lgname": "bot_username",
-    "lgpassword":"bot_password",
-    "format":"json",
-    "lgtoken": LOGIN_TOKEN
-}
+    Stash mode is used to build a file up in pieces and then commit it at the end
+    """
 
-R = S.post(URL, data=PARAMS_2)
+    chunks = read_chunks(FILE)
+    chunk = next(chunks)
 
-# Step 3: While logged in, retrieve a CSRF token
-PARAMS_3 = {
-    "action": "query",
-    "meta":"tokens",
-    "format":"json"
-}
-
-R = S.get(url=URL, params=PARAMS_3)
-DATA = R.json()
-CSRF_TOKEN = DATA["query"]["tokens"]["csrftoken"]
-
-# Step 4: Post requests to upload a file in chunks using "stash" mode,
-# to build a file up in pieces and then commit it at the end.
-
-# filepath contains the path of the file's location
-FILE_PATH = 'f.jpg'
-FILE = open(FILE_PATH, 'rb')
-FILE_SIZE = os.stat(FILE_PATH).st_size
-CHUNKS = read_chunks(FILE)
-CHUNK = next(CHUNKS)
-
-#parameters for the first chunk
-PARAMS_4 = {
-    "action": "upload",
-    "stash": 1,
-    "filename": "chunk_test.jpg",
-    "filesize": FILE_SIZE,
-    "offset": 0,
-    "format": "json",
-    "token": CSRF_TOKEN,
-    "ignorewarnings": 1
-}
-
-INDEX = 0
-FILE = {'chunk':('{}.jpg'.format(INDEX), CHUNK, 'multipart/form-data')}
-INDEX += 1
-R = S.post(URL, files=FILE, data=PARAMS_4)
-DATA = R.json()
-print(DATA)
-
-#For the second and further chunks, pass in the filekey parameter as well
-for chunk in CHUNKS:
-    PARAMS_4 = {
+    #parameters for the first chunk
+    params = {
         "action": "upload",
         "stash": 1,
-        "offset": DATA["upload"]["offset"],
         "filename": "chunk_test.jpg",
         "filesize": FILE_SIZE,
-        "filekey": DATA["upload"]["filekey"],
+        "offset": 0,
         "format": "json",
-        "token": CSRF_TOKEN,
+        "token": csrf_token,
         "ignorewarnings": 1
     }
-    FILE = {'chunk':('{}.jpg'.format(INDEX), chunk, 'multipart/form-data')}
-    INDEX += 1
-    R = S.post(URL, files=FILE, data=PARAMS_4)
-    DATA = R.json()
-    print(DATA)
+    index = 0
+    file = {'chunk':('{}.jpg'.format(index), chunk, 'multipart/form-data')}
+    index += 1
+    res = S.post(URL, files=file, data=params)
+    data = res.json()
+    print(data)
 
-#Final upload using the filekey to commit the upload out of the stash area
-PARAMS_5 = {
-    "action": "upload",
-    "filename": "chunk_test.jpg",
-    "filekey": DATA["upload"]["filekey"],
-    "format": "json",
-    "comment": "Upload Testing",
-    "token": CSRF_TOKEN,
-}
+    #For the second and further chunks, pass in the filekey parameter as well
+    for chunk in chunks:
+        params = {
+            "action": "upload",
+            "stash": 1,
+            "offset": data["upload"]["offset"],
+            "filename": "chunk_test.jpg",
+            "filesize": FILE_SIZE,
+            "filekey": data["upload"]["filekey"],
+            "format": "json",
+            "token": csrf_token,
+            "ignorewarnings": 1
+        }
+        file = {'chunk':('{}.jpg'.format(index), chunk, 'multipart/form-data')}
+        index += 1
+        res = S.post(URL, files=file, data=params)
+        data = res.json()
+        print(data)
 
-R = S.post(URL, data=PARAMS_5)
-DATA = R.json()
-print(DATA)
+    #Final upload using the filekey to commit the upload out of the stash area
+    params = {
+        "action": "upload",
+        "filename": "chunk_test.jpg",
+        "filekey": data["upload"]["filekey"],
+        "format": "json",
+        "comment": "Upload Testing",
+        "token": csrf_token,
+    }
+    res = S.post(URL, data=params)
+    data = res.json()
+    print(data)
+
+def main():
+    """ Login to a user account and upload a file in chunks."""
+    login_token = fetch_login_token()
+    user_login(login_token)
+    csrf_token = fetch_csrf_token()
+    upload_file_in_chunks(csrf_token)
+
+
+if __name__ == "__main__":
+    main()
