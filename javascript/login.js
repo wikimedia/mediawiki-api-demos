5e9bdb2e798b10810b68daeb402962b39f7434a6
sumanth699 <46819006+sumanth699@users.noreply.github.com>
2019-03-28 18:28:19 +0530

    Update login.js
    changed the format from ajax to node

--- javascript/login.js
+++ javascript/login.js
@@ -1,48 +1,67 @@
-/*
+/* 
     login.js
     MediaWiki Action API Code Samples
     Demo of `Login` module: Sending post request to login
     MIT license
 */
+var fetch = require('node-fetch');
+var url = "https://mediawiki.org/w/api.php";
+url = url + "?";
+var cookie;
 
-//Function to convert the dictionary element as an encoded URL
+// Step 1: GET Request to fetch login token
+function getLoginToken() {
+    var PARAMS_0 = {
+        action: "query",
+        meta: "tokens",
+        type: "login",
+        format: "json"
+    };
 
-function conv(parm) {
-  var str = [];
-  for(var p in parm)
-     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(parm[p]));
-  return str.join("&");
+    var query = url;
+    Object.keys(PARAMS_0).forEach(function (key) {
+        query += "&" + key + "=" + PARAMS_0[key];
+    });
+
+    fetch(query)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            doLogin(data.query.tokens.logintoken);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
-const URL = "https://mediawiki.org/w/api.php";
-
-//Retrieve login token first
-
-var PARAMS_0 = {'action':"query",'meta':"tokens",'type':"login",'format':"json"};
-var url = URL+"?"+conv(PARAMS_0);
-var S = new XMLHttpRequest();
-S.open("GET",url);
-S.send();
-S.onreadystatechange = function(){
-     if(S.status==200 && S.readyState==4){
-	      var R = S.responseText;
-		  var DATA = JSON.parse(R);
-		  var LOGIN_TOKEN = DATA.query.tokens.logintoken;
-		  console.log(LOGIN_TOKEN);
-	 }
+// Step 2: POST Request to log in. 
+// Use of main account for login is not
+// supported. Obtain credentials via Special:BotPasswords
+// (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+function doLogin(LOGIN_TOKEN){
+	var PARAMS_1 = {
+    action:"login",
+    lgname:"your_bot_username",
+    lgpassword:"your_bot_password",
+    lgtoken:LOGIN_TOKEN,
+    format:"json"
 };
-
-/*Send a post request to login. Using the main account for login is not supported. 
-Obtain credentials via Special:BotPasswords(https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword*/
-
-var PARAMS_1 = {'action':"login",'lgname':"your_bot_username",'lgpassword':"your_bot_password",'lgtoken':LOGIN_TOKEN,'format':"json"};
-var S1 = new XMLHttpRequest();
-S1.open("POST",URL);
-S1.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-S1.send(conv(PARAMS_1));  //Sending POST request with encoded elements
-S1.onreadystatechange = function(){
-     if(S1.status==200 && S1.readyState==4){
-	      var R = S1.responseText;
-		  var DATA = JSON.parse(R);
-		  console.log(DATA);
-	 }
-};
+  fetch(url,{credentials:"include",
+              method:"POST",
+			  body:JSON.stringify(PARAMS_1),
+			  header:{
+				  "Content-type":"application/json"
+			  }
+  })
+  .then(function(response){
+	  var setCookie = response.headers.get('set-cookie');
+      cookie = setCookie.substr(0, setCookie.indexOf(';'));
+	  return response.json();
+  })
+   .then(function (data) {
+            console.log(data)
+    })
+  .catch(function (error){
+	  console.log(error);
+  });
+  }
