33a986c4c9e88e70d6f0a2b6ac3625241bb11698
sumanth699 <46819006+sumanth699@users.noreply.github.com>
2019-03-28 18:13:55 +0530

    Update watch.js
    changed the format from ajax to node

--- javascript/watch.js
+++ javascript/watch.js
@@ -1,73 +1,117 @@
 /*
-   watch.js
+watch.js
     MediaWiki Action API Code Samples
     Demo of `Watch` module: Add a page to your watchlist
     MIT license
 */
+var fetch = require('node-fetch');
+var url = "https://en.wikipedia.org/w/api.php";
+url = url + "?";
+var cookie;
 
-//Function to convert the dictionary element as an encoded URL
-
-function conv(parm) {
-  var str = [];
-  for(var p in parm)
-     str.push(encodeURIComponent(p) + "=" + encodeURIComponent(parm[p]));
-  return str.join("&");
-}
-const URL = "https://test.wikipedia.org/w/api.php";
-
-//Retrieve login token first
-
-var PARAMS_1 = {'action':"query",'meta':"tokens",'type':"login",'format':"json"};
-var url = URL+"?"+conv(PARAMS_1);
-var S = new XMLHttpRequest();
-S.open("GET",url);
-S.send();
-S.onreadystatechange = function(){
-     if(S.status==200 && S.readyState==4){
-	      var R = S.responseText;
-		  var DATA = JSON.parse(R);
-		  var LOGIN_TOKEN = DATA.query.tokens.logintoken;
-		  console.log(LOGIN_TOKEN);
-	 }
-};
-/*# Step 2: Send a post request to log in. For this loginmethod, Obtain credentials by first visiting
-https://www.en.wikipedia.org/wiki/Special:BotPasswords
-See https://www.mediawiki.org/wiki/API:Login for more information on log in methods.*/
-
-var PARAMS_2 = {'action':"login",'lgname':"username",'lgpassword':"password",'format':"json",'lgtoken':LOGIN_TOKEN};
-var S1 = new XMLHttpRequest();
-S1.open("POST",URL);
-S1.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-S1.send(conv(PARAMS_2));  //Sending POST request with encoded elements
-S1.onreadystatechange = function(){
-     if(S1.status==200 && S1.readyState==4){
-	      //Step 3: While logged in, retrieve a CSRF token
-		  
-          var PARAMS_3 = {"action": "query","meta": "tokens","type": "watch","format": "json"};
-          var url = URL+"?"+conv(PARAMS_3);
-          var S2 = new XMLHttpRequest();
-          S2.open("GET",url);
-          S2.send();
-          S2.onreadystatechange = function(){
-                 if(S2.status==200 && S2.readyState==4){
-	             var R = S2.responseText;
-		         var DATA = JSON.parse(R);
-		         var CSRF_TOKEN = DATA.query.tokens.watchtoken;
-	            }
-            };
-			//Step 4: Post request to add a page to your watchlist
-			
-			PARAMS_4 = {"action": "watch","titles": "Stone forest","format": "json","token": CSRF_TOKEN,};
-			var S3 = new XMLHttpRequest();
-			S3.open("POST",URL);
-            S3.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-            S3.send(conv(PARAMS_4));  //Sending POST request with encoded elements
-			S3.onreadystatechange = function(){
-				if(S1.status==200 && S1.readyState==4){
-					var R = S3.responseText;
-					var DATA = JSON.parse(R);
-					console.log(DATA);
-				}
-			}
-	    }
+// Step 1: GET Request to fetch login token
+function getLoginToken() {
+    var PARAMS_1 = {
+        action: "query",
+        meta: "tokens",
+        type: "login",
+        format: "json"
     };
+
+    var query = url;
+    Object.keys(PARAMS_1).forEach(function (key) {
+        query += "&" + key + "=" + PARAMS_1[key];
+    });
+
+    fetch(query)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            doLogin(data.query.tokens.logintoken);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+/*Step 2: Send a post request to log in. For this login method, Obtain credentials by first visiting https://www.en.wikipedia.org/wiki/Special:BotPasswords
+See https://www.mediawiki.org/wiki/API:Login for more information on log in methods.*/
+function doLogin(LOGIN_TOKEN){
+	var PARAMS_2 = {
+    action:"login",
+    lgname:"your_bot_username",
+    lgpassword:"your_bot_password",
+    lgtoken:LOGIN_TOKEN,
+    format:"json"
+};
+  fetch(url,{credentials:"include",
+              method:"POST",
+			  body:JSON.stringify(PARAMS_2),
+			  header:{
+				  "Content-type":"application/json"
+			  }
+  })
+  .then(function(response){
+	  var setCookie = response.headers.get('set-cookie');
+      cookie = setCookie.substr(0, setCookie.indexOf(';'));
+	  getCsrfToken();
+  })
+  .catch(function (error){
+	  console.log(error);
+  });
+  }
+  //retrieve a CSRF token
+ function getCsrfToken(){
+	 var PARAMS_3 = {
+    action: "query",
+    meta: "tokens",
+    type: "watch",
+    format: "json"
+};
+  var query = url;
+    Object.keys(PARAMS_3).forEach(function (key) {
+        query += "&" + key + "=" + PARAMS_3[key];
+    });
+	fetch(query, {
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Cookie": cookie,
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            addPage(data.query.tokens.watchtoken);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+ }
+ //Post request to add a page to your watchlist
+ function addPage(CSRF_TOKEN){
+	 var PARAMS_4 = {
+    action: "watch",
+    titles: "Stone forest",
+    format: "json",
+    token: CSRF_TOKEN,
+}
+fetch(url,{credentials: "include",
+            method: "POST",
+            body: JSON.stringify(PARAMS_4),
+            headers: {
+                "Content-Type": "application/json",
+                "Cookie": cookie,
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+ }
