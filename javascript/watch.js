237d150c4d52d9fd96274e1f8d494f5fc8fd8fed
Venkata Sai Sumanth Javvaji <saisumanth532@gmail.com>
2019-05-03 12:47:43 +0530

    Update watch.js
    corrected the minor fixes suggested

--- javascript/watch.js
+++ javascript/watch.js
@@ -1,29 +1,30 @@
 /*
-watch.js
+    watch.js
     MediaWiki Action API Code Samples
     Demo of `Watch` module: Add a page to your watchlist
     MIT license
 */
-var fetch = require('node-fetch');
+
+var request = require('node-request').defaults({jar: trye});
 var url = "https://en.wikipedia.org/w/api.php";
 url = url + "?";
 var cookie;
 
-// Step 1: GET Request to fetch login token
+// Step 1: GET Request to request login token
 function getLoginToken() {
-    var PARAMS_1 = {
-        action: "query",
-        meta: "tokens",
-        type: "login",
-        format: "json"
-    };
+	 var PARAMS_1 = {
+             action: "query",
+             meta: "tokens",
+             type: "login",
+             format: "json"
+          };
 
     var query = url;
     Object.keys(PARAMS_1).forEach(function (key) {
         query += "&" + key + "=" + PARAMS_1[key];
     });
 
-    fetch(query)
+    request.get(query)
         .then(function (response) {
             return response.json();
         })
@@ -38,17 +39,17 @@ function getLoginToken() {
 See https://www.mediawiki.org/wiki/API:Login for more information on log in methods.*/
 function doLogin(LOGIN_TOKEN){
 	var PARAMS_2 = {
-    action:"login",
-    lgname:"your_bot_username",
-    lgpassword:"your_bot_password",
-    lgtoken:LOGIN_TOKEN,
-    format:"json"
-};
-  fetch(url,{credentials:"include",
+    	    action:"login",
+	    lgname:"your_bot_username",
+    	    lgpassword:"your_bot_password",
+    	    lgtoken:LOGIN_TOKEN,
+    	    format:"json"
+         };
+  request.post(url,{credentials:"include",
               method:"POST",
-			  body:JSON.stringify(PARAMS_2),
-			  header:{
-				  "Content-type":"application/json"
+	      body:JSON.stringify(PARAMS_2),
+	      header:{
+	      		"Content-type":"application/json"
 			  }
   })
   .then(function(response){
@@ -63,16 +64,16 @@ function doLogin(LOGIN_TOKEN){
   //retrieve a CSRF token
  function getCsrfToken(){
 	 var PARAMS_3 = {
-    action: "query",
-    meta: "tokens",
-    type: "watch",
-    format: "json"
-};
+             action: "query",
+             meta: "tokens",
+             type: "watch",
+             format: "json"
+         };
   var query = url;
     Object.keys(PARAMS_3).forEach(function (key) {
         query += "&" + key + "=" + PARAMS_3[key];
     });
-	fetch(query, {
+	request.get(query, {
             credentials: "include",
             headers: {
                 "Content-Type": "application/json",
@@ -92,12 +93,12 @@ function doLogin(LOGIN_TOKEN){
  //Post request to add a page to your watchlist
  function addPage(CSRF_TOKEN){
 	 var PARAMS_4 = {
-    action: "watch",
-    titles: "Stone forest",
-    format: "json",
-    token: CSRF_TOKEN,
+             action: "watch",
+             titles: "Stone forest",
+             format: "json",
+             token: CSRF_TOKEN,
 }
-fetch(url,{credentials: "include",
+request.post(url,{credentials: "include",
             method: "POST",
             body: JSON.stringify(PARAMS_4),
             headers: {
@@ -115,3 +116,5 @@ fetch(url,{credentials: "include",
             console.log(error);
         });
  }
+// Start From Step 1
+getLoginToken();
