555e02860e5a31efbc1e3e37be5b2bb642d4d56c
Venkata Sai Sumanth Javvaji <saisumanth532@gmail.com>
2019-05-03 12:42:50 +0530

    Update login.js
    fixed the errors suggested

--- javascript/login.js
+++ javascript/login.js
@@ -1,10 +1,10 @@
-/* 
+/*
     login.js
     MediaWiki Action API Code Samples
     Demo of `Login` module: Sending post request to login
     MIT license
 */
-var fetch = require('node-fetch');
+var request = require('node-request').defaults({jar:true});
 var url = "https://mediawiki.org/w/api.php";
 url = url + "?";
 var cookie;
@@ -23,7 +23,7 @@ function getLoginToken() {
         query += "&" + key + "=" + PARAMS_0[key];
     });
 
-    fetch(query)
+    request.get(query)
         .then(function (response) {
             return response.json();
         })
@@ -37,20 +37,20 @@ function getLoginToken() {
 // Step 2: POST Request to log in. 
 // Use of main account for login is not
 // supported. Obtain credentials via Special:BotPasswords
-// (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+// (https://www.mediawiki.org/wiki/Special:BotPasswords)
 function doLogin(LOGIN_TOKEN){
 	var PARAMS_1 = {
-    action:"login",
-    lgname:"your_bot_username",
-    lgpassword:"your_bot_password",
-    lgtoken:LOGIN_TOKEN,
-    format:"json"
-};
-  fetch(url,{credentials:"include",
+    	    action:"login",
+            lgname:"your_bot_username",
+            lgpassword:"your_bot_password",
+            lgtoken:LOGIN_TOKEN,
+            format:"json"
+         };
+  request.post(url,{credentials:"include",
               method:"POST",
-			  body:JSON.stringify(PARAMS_1),
-			  header:{
-				  "Content-type":"application/json"
+	      body:JSON.stringify(PARAMS_1),
+	      header:{
+			"Content-type":"application/json"
 			  }
   })
   .then(function(response){
@@ -65,3 +65,5 @@ function doLogin(LOGIN_TOKEN){
 	  console.log(error);
   });
   }
+// Start From Step 1
+getLoginToken();
