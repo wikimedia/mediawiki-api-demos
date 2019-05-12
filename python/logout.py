8aaa789c8bcdbfb5ed0313a4372d190af420bc91
Jayprakash12345 <0freerunning@gmail.com>
2019-05-12 18:51:30 +0530

    Rewrite the logout module
    * Remove logout module from modules.json
    * Rewrite logout.py
    * Rewrite logout.js

--- python/logout.py
+++ python/logout.py
@@ -1,5 +1,3 @@
-#This file is auto-generated. See modules.json and autogenerator.py for details
-
 #!/usr/bin/python3
 
 """
@@ -17,12 +15,62 @@ S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
-PARAMS = {
-    "action": "logout",
-    "format": "json"
+# Retrieve login token first
+PARAMS_0 = {
+    'action':"query",
+    'meta':"tokens",
+    'type':"login",
+    'format':"json"
 }
 
-R = S.get(url=URL, params=PARAMS)
+R = S.get(url=URL, params=PARAMS_0)
+DATA = R.json()
+
+LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+print(LOGIN_TOKEN)
+
+# Send a POST request to login. Using the main account for login is not
+# supported. Obtain credentials via Special:BotPasswords
+# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+
+PARAMS_1 = {
+    'action':"login",
+    'lgname':"your_bot_username",
+    'lgpassword':"your_bot_password",
+    'lgtoken':LOGIN_TOKEN,
+    'format':"json"
+}
+
+R = S.post(URL, data=PARAMS_1)
 DATA = R.json()
 
 print(DATA)
+
+# GET request to fetch CSRF token
+PARAMS_2 = {
+    "action":"query",
+    "meta":"tokens",
+    "format":"json"
+}
+
+R = S.get(url=URL, params=PARAMS_2)
+DATA = R.json()
+
+CSRF_TOKEN = DATA['query']['tokens']['csrftoken']
+
+print( CSRF_TOKEN )
+
+# Send a POST request to logout.
+PARAMS_3 = {
+    "action": "logout",
+    "token": CSRF_TOKEN,
+    "format": "json"
+}
+
+R = S.post(URL, data=PARAMS_3)
+DATA = R.json()
+
+# Check weither the response is null or not
+if DATA == {}:
+    print("You have successfully logout.")
