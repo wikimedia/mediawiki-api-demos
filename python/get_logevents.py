0f117ccc0bf8b778db2d0024554131f7f434a540
brendajerop <brendajerop2@gmail.com>
2019-06-06 19:04:28 +0300

    Add sample code for API:Logevents to get the three most-recent logevents

---
+++ python/get_logevents.py
@@ -0,0 +1,30 @@
+#This file is auto-generated. See modules.json and autogenerator.py for details
+
+#!/usr/bin/python3
+
+"""
+    python/get_logevents.py
+
+    MediaWiki API Demos
+    Demo of `Logevents` module: Get the three most recent logevents.
+
+    MIT License
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "query",
+    "format": "json",
+    "list": "logevents",
+    "lelimit": "3"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
