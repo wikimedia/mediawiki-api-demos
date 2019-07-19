6da8c9ace3267a4c5a7842197c1c43f40f429b4a
Jayprakash12345 <0freerunning@gmail.com>
2019-07-20 02:16:00 +0530

    Generate sample code for MediaWikiJS

---
+++ mediawikijs/edit.js
@@ -0,0 +1,20 @@
+/*
+	edit.js
+
+	MediaWiki API Demos
+	Demo of `Edit` module: POST request to edit a page
+
+	MIT License
+*/
+
+var params = {
+		action: 'edit',
+		title: 'Sandbox',
+		appendtext: 'Hello',
+		format: 'json'
+	},
+	api = new mw.Api();
+
+api.postWithToken( 'csrf', params ).done( function ( data ) {
+	console.log( data );
+} );
