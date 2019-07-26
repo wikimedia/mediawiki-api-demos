3be1c8b32c563c6e82d4ae585d0eb7833e67f216
Jayprakash12345 <0freerunning@gmail.com>
2019-07-27 04:19:23 +0530

    Added 4 GET file in modules.json

--- python/get_recent_changes.py
+++ python/get_recent_changes.py
@@ -29,4 +29,7 @@ PARAMS = {
 R = S.get(url=URL, params=PARAMS)
 DATA = R.json()
 
-print(DATA)
+RECENTCHANGES = DATA['query']['recentchanges']
+
+for rc in RECENTCHANGES:
+    print(str(rc['title']))
