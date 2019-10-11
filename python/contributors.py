2799a6d93e3123313c11bb1c9b4a72b5e0136752
tybanash <awahnadege02@gmail.com>
2019-10-11 06:34:22 -0700

    Added sample codes to API:Contributors

---
+++ python/contributors.py
@@ -0,0 +1,34 @@
+#This file is auto-generated. See modules.json and autogenerator.py for details
+
+#!/usr/bin/python3
+
+"""
+    get_contributors.py
+
+    MediaWiki API Demos
+    Demo of `Contributors` module: Get request to list all logged-in contributors and count of anonymous contributors to a page.
+
+    MIT License
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "query",
+    "titles": "MediaWiki",
+    "prop": "contributors",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+PAGES = DATA["query"]["pages"]
+
+for page in PAGES:
+    print(DATA)
+
