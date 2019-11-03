885e32b0ddd44f4d7618e68751eff1b0d9d88c1c
MinervaLong <vanesa.perea.cuevas@gmail.com>
2019-11-03 18:21:23 +0100

    Add login code to js file

--- javascript/get_deleted_revs.js
+++ javascript/get_deleted_revs.js
@@ -7,9 +7,55 @@
     MIT License
 */
 
+var request = require('request').defaults({jar: true}),
+    url = "https://en.wikipedia.org/w/api.php";
+
+// Step 1: GET Request to fetch login token
+
+function getLoginToken() {
+    var params_0 = {
+        action: "query",
+        meta: "tokens",
+        type: "login",
+        format: "json"
+    };
+
+    request.get({ url: url, qs: params_0 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        var data = JSON.parse(body);
+        loginRequest(data.query.tokens.logintoken);
+    });
+}
+
+// Step 2: POST Request to log in.
+// Use of main account for login is not
+// supported. Obtain credentials via Special:BotPasswords
+// (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+
+function loginRequest(login_token) {
+    var params_1 = {
+        action: "login",
+        lgname: "bot_username",
+        lgpassword: "bot_password",
+        lgtoken: login_token,
+        format: "json"
+    };
+
+    request.post({ url: url, form: params_1 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        deleted_revisions();
+    });
+}
+
+// Step 3: GET request to get the deleted revisions
+
 var url = "https://en.wikipedia.org/w/api.php"; 
 
-var params = {
+var params_2 = {
     action: "query",
     format: "json",
     titles: "Talk:MainPage",
@@ -18,7 +64,7 @@ var params = {
 };
 
 url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+Object.keys(params_2).forEach(function(key){url += "&" + key + "=" + params_2[key];});
 
 fetch(url)
     .then(function(response){return response.json();})
