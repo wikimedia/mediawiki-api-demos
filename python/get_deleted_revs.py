7c7f48f84128ec84be6cc7d276cad118bbc3d72c
MinervaLong <vanesa.perea.cuevas@gmail.com>
2019-11-03 18:37:17 +0100

    Add login code python file

--- python/get_deleted_revs.py
+++ python/get_deleted_revs.py
@@ -6,13 +6,45 @@
     MIT license
 """
 
+
 import requests
 
 S = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
-PARAMS = {
+# Retrieve login token
+
+PARAMS_0 = {
+    'action':"query",
+    'meta':"tokens",
+    'type':"login",
+    'format':"json"
+}
+
+R = S.get(url=URL, params=PARAMS_0)
+DATA = R.json()
+
+LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+# Send a post request to login. Using the main account for login is not
+# supported. Obtain credentials via Special:BotPasswords
+# (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+
+PARAMS_1 = {
+    'action':"login",
+    'lgname':"your_bot_username",
+    'lgpassword':"your_bot_password",
+    'lgtoken':LOGIN_TOKEN,
+    'format':"json"
+}
+
+R = S.post(URL, data=PARAMS_1)
+
+#Send a get request to get the list of deleted revisions
+
+
+PARAMS_2 = {
     'action':"query",
     'format':"json",
     'titles':"Talk:MainPage",
@@ -20,7 +52,7 @@ PARAMS = {
     'drv':"prop"
 }
 
-R = S.get(url=URL, params=PARAMS)
+R = S.get(url=URL, params=PARAMS_2)
 DATA = R.json()
 
 print(DATA)
