ec33383154630277cc55d83c6868608eaea6abb5
MinervaLong <vanesa.perea.cuevas@gmail.com>
2019-11-15 22:22:18 +0100

    Change module name in README file

--- mediawikijs/get_deleted_revs.js
+++ mediawikijs/get_deleted_revs.js
@@ -2,7 +2,7 @@
 	get_deleted_revs.js
 
 	MediaWiki API Demos
-	Demo of `Deleted revisions:Get a list of deleted revision for Talk:Main Page` module
+	Demo of `Deleted Revisions module:Get a list of deleted revision for a page` module
 
 	MIT License
 */
