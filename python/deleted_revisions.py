8a6c513489f0c044d759ebd5fddde44c066eace9
zainab.abubakar <abuzain1995@gmail.com>
2020-01-20 10:22:36 +0100

    Added login to sample codes for API:Deletedrevisions

--- python/deleted_revisions.py
+++ python/deleted_revisions.py
@@ -1,5 +1,3 @@
-#This file is auto-generated. See modules.json and autogenerator.py for details
-
 #!/usr/bin/python3
 
 """
@@ -15,8 +13,38 @@ import requests
 
 S = requests.Session()
 
-URL = "https://en.wikipedia.org/w/api.php"
+URL = "https://test.wikipedia.org/w/api.php"
 
+# Step 1: Retrieve a login token
+PARAMS_1 = {
+    "action": "query",
+    "meta": "tokens",
+    "type": "login",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS_1)
+DATA = R.json()
+
+LOGIN_TOKEN = DATA['query']['tokens']['logintoken']
+
+# Step 2: Send a POST request to log in. For this login
+# method, obtain credentials by first visiting
+# https://www.test.wikipedia.org/wiki/Manual:Bot_passwords
+# See https://www.mediawiki.org/wiki/API:Login for more
+# information on log in methods.
+PARAMS_2 = {
+    "action": "login",
+    "lgname": "user_name",
+    "lgpassword": "password",
+    "format": "json",
+    "lgtoken": LOGIN_TOKEN
+}
+
+R = S.post(URL, data=PARAMS_2)
+DATA = R.json()
+
+# Step 3: While logged in, send a get reguest to get a list of deleted revisions for Talk:Main Page.
 PARAMS = {
     "action": "query",
     "titles": "Talk:Main_Page",
