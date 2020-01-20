2f9ecc785a005e67dbf5d045805a6dc0ffef8443
zainab.abubakar <abuzain1995@gmail.com>
2020-01-20 10:22:36 +0100

    Added login to sample codes for API:Deletedrevisions

--- javascript/deleted_revisions.js
+++ javascript/deleted_revisions.js
@@ -9,21 +9,64 @@
     MIT License
 */
 
-var url = "https://en.wikipedia.org/w/api.php"; 
+var request = require("request").defaults({jar: true}),
+url = "https://en.wikipedia.org/w/api.php";
 
-var params = {
-    action: "query",
-    titles: "Talk:Main_Page",
-    prop: "deletedrevisions",
-    drvprop: "user|comment|content",
-    drvslots: "*",
-    format: "json"
-};
+// Step 1: GET Request to fetch login token
+function getLoginToken() {
+    var params_0 = {
+        action: "query",
+        meta: "tokens",
+        type: "login",
+        format: "json"
+    };
+    request.get({ url: url, qs: params_0 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        var data = JSON.parse(body);
+        loginRequest(data.query.tokens.logintoken);
+    });
+}
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+// Step 2: POST request to log in. 
+// Use of main account for login is not
+// supported. Obtain credentials via Special:BotPasswords
+// (https://www.mediawiki.org/wiki/Special:BotPasswords) for lgname & lgpassword
+function loginRequest(login_token) {
+    var params_1 = {
+        action: "login",
+        lgname: "bot_username",
+        lgpassword: "bot_password",
+        lgtoken: login_token,
+        format: "json"
+    };
+    
+    request.post({ url: url, form: params_1 }, function (error, res, body) {
+        if (error) {
+            return;
+        }
+        deletedRevisions();
+    });
+}
 
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {console.log(response);})
-    .catch(function(error){console.log(error);});
+// Step 3: Get a list of deleted revisions for Talk:Main Page.
+function deletedRevisions() {
+
+    var params = {
+        action: "query",
+        titles: "Talk:Main_Page",
+        prop: "deletedrevisions",
+        drvprop: "user|comment|content",
+        drvslots: "*",
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {console.log(response);})
+        .catch(function(error){console.log(error);});
+}
