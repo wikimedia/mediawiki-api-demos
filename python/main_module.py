c5fb3e696b3ca1b7aadeb59e60a16850069ffc81
zainab.abubakar <abuzain1995@gmail.com>
2020-02-06 14:23:58 +0100

    Added Sample codes for API:Main_module

---
+++ python/main_module.py
@@ -0,0 +1,28 @@
+#This file is auto-generated. See modules.json and autogenerator.py for details
+
+#!/usr/bin/python3
+
+"""
+    main_module.py
+
+    MediaWiki API Demos
+    Demo of `Main module` module: Get help for the main module.
+
+    MIT License
+"""
+
+import requests
+
+S = requests.Session()
+
+URL = "https://en.wikipedia.org/w/api.php"
+
+PARAMS = {
+    "action": "help",
+    "format": "json"
+}
+
+R = S.get(url=URL, params=PARAMS)
+DATA = R.json()
+
+print(DATA)
